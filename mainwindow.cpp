d8028cde2ff5d48125e8e98b598a100514e739b0
hamed <hameddev@gmail.com>
2026-05-23 19:20:17 +0330

    درون برنچ کامنت نوشتم

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    testGitFunction();
+
+    /// برنچ myhamed
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::testGitFunction()
+{
+    qDebug() << "این تابع برای تست Git ساخته شده است";
+}
+
+
+
